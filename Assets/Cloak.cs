28878ef6c15bdd1ee532cf259cd87fccaac98daf
RiguelCardona <riguelcardona@gmail.com>
2020-10-01 21:59:14 -0300

    Plantillas y particulas

---
+++ Assets/Cloak.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cloak : MonoBehaviour
+{
+     public GameObject cloak;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector2(.5f,.5f);
+    }
+}
